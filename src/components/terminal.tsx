c2f5c09a14e014c99a3a231b74c39cc9f85b676a
TakatokiOshima <ki13sd6p@gmail.com>
2022-06-28 21:14:46 +0900

    add: 自動フォーカス

--- src/components/terminal.tsx
+++ src/components/terminal.tsx
@@ -1,11 +1,14 @@
-import { KeyboardEvent, useRef, useState } from "react"
+import { KeyboardEvent, useEffect, useRef, useState } from "react"
 import styles from "../styles/styles.module.css"
 
 export default function Terminal() {
-  const ARROW_TEXT = "> ";
-  const ref = useRef<HTMLInputElement>(null);
+  const ARROW_TEXT = "> "
+  const ref = useRef<HTMLInputElement>(null)
   const [text, setText] = useState<string>("")
-  const [lines, setLines] = useState<string[]>([])
+  const [lines, setLines] = useState<string[]>([
+    "webcli",
+    "Powered by 0shim4."
+  ])
 
   const onKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
@@ -19,6 +22,11 @@ export default function Terminal() {
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value.substring(2))
   }
+  useEffect(() => {
+    if (lines.length > 8) {
+      ref?.current?.scrollIntoView()
+    }
+  })
     
   return (
     <div className={styles.terminal}>
