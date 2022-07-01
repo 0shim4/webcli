6481884104ff8c0963a98243a57e5301026be841
TakatokiOshima <ki13sd6p@gmail.com>
2022-07-01 20:43:30 +0900

    add: なんかやってるっぽく

--- src/components/terminal.tsx
+++ src/components/terminal.tsx
@@ -2,49 +2,71 @@ import { KeyboardEvent, useEffect, useRef, useState } from "react"
 import styles from "../styles/styles.module.css"
 
 export default function Terminal() {
-  const ARROW_TEXT = "> "
   const ref = useRef<HTMLInputElement>(null)
   const [text, setText] = useState<string>("")
   const [lines, setLines] = useState<string[]>([
     "webcli",
     "Powered by 0shim4."
   ])
+  const ramdomResponse = [
+    {
+      text: [
+        "* Is's Test Message.",
+        "* Please enter freely."
+      ],
+      input: ""
+    },
+    {
+      text: [
+        "* I'm speaking.",
+      ],
+      input: "* Listen to me!"
+    }
+  ]
 
   const onKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
-      setLines([...lines, ARROW_TEXT + text])
-      if (text === "clear" || text === "cls") {
-        setLines([])
+      var response = ramdomResponse[Math.floor(Math.random()*ramdomResponse.length)]
+      setLines([...lines, "> " + text])
+      if (text.length > 0) {
+        setLines([...lines, "> " + text, ...response.text])
       }
-      setText("")
+      setText(response.input)
     }
   }
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setText(e.target.value.substring(2))
+    setText(e.target.value)
   }
   useEffect(() => {
     if (lines.length > 8) {
       ref?.current?.scrollIntoView()
     }
   })
-    
+
   return (
     <div className={styles.terminal}>
       {lines.map((line, index) => <input
         readOnly={true}
         key={String(index)}
         value={line}
-        className={styles.inputLine}
+        className={styles.displayLine}
       />)}
-      <input
-        ref={ref}
-        className={styles.inputLine}
-        value={ARROW_TEXT + text}
-        onKeyPress={(e) => onKeyPress(e)}
-        onChange={(e) => onChange(e)}
-        onBlur={(e) => ref.current?.focus()}
-        autoFocus={true}
-      />
+      <div className={styles.line}>
+        <input
+          readOnly={true}
+          value={"> "}
+          className={styles.arrow}
+        />
+        <input
+          ref={ref}
+          className={styles.inputLine}
+          value={text}
+          onKeyPress={(e) => onKeyPress(e)}
+          onChange={(e) => onChange(e)}
+          onBlur={(e) => ref.current?.focus()}
+          autoFocus={true}
+        />
+      </div>
     </div>
-    )
+  )
 }
